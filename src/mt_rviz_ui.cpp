3287145b6c7528b456f57473a750dff3ead5c83c
RaviRoKes <keshari.ravi2086@gmail.com>
2024-11-29 16:42:48 +0100

    nothing

--- src/mt_rviz_ui.cpp
+++ src/mt_rviz_ui.cpp
@@ -1,3 +1,17 @@
+// Copyright (c) 2023, Stogl Robotics Consulting UG (haftungsbeschränkt)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include <custom_interactive_markers/mt_rviz_ui.hpp>
 #include <rviz_common/logging.hpp>
 #include <rviz_common/properties/string_property.hpp>
@@ -72,7 +86,6 @@ namespace custom_interactive_markers
         rviz_common::Panel::onInitialize();
         RCLCPP_INFO(node_->get_logger(), "MTRvizUI plugin initialized.");
     }
-
 
     void MTRvizUI::createInteractiveMarkers()
     {
@@ -156,9 +169,6 @@ namespace custom_interactive_markers
         // Add cylinders as interactive markers
         addCylinders();
     }
-
-
-
     void MTRvizUI::addCylinders()
     {
         double radius = radius_input_->value();
